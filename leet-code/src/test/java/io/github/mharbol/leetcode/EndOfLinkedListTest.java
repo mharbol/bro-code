74b41da5775978e7d780207913ec84f33099c87e
Marcus Harbol <mharbol@users.noreply.github.com>
2023-09-22 16:18:44 -0400

    transition to static ListNode generator

--- leet-code/src/test/java/io/github/mharbol/leetcode/EndOfLinkedListTest.java
+++ leet-code/src/test/java/io/github/mharbol/leetcode/EndOfLinkedListTest.java
@@ -15,16 +15,9 @@ public class EndOfLinkedListTest {
 
     @Test
     public void testLeetCode1() {
-        ListNode node5 = new ListNode(5);
-        ListNode node4 = new ListNode(4, node5);
-        ListNode node3 = new ListNode(3, node4);
-        ListNode node2 = new ListNode(2, node3);
-        ListNode node1 = new ListNode(1, node2);
+        ListNode node1 = ListNode.newList(1, 2, 3, 4, 5);
 
-        ListNode expected5 = new ListNode(5);
-        ListNode expected3 = new ListNode(3, expected5);
-        ListNode expected2 = new ListNode(2, expected3);
-        ListNode expected = new ListNode(1, expected2);
+        ListNode expected = ListNode.newList(1, 2, 3, 5);
 
         ListNode actual = cut.removeNthFromEnd(node1, 2);
 
@@ -44,8 +37,7 @@ public class EndOfLinkedListTest {
     @Test
     public void testLeetCode3() {
 
-        ListNode node2 = new ListNode(2);
-        ListNode node1 = new ListNode(1, node2);
+        ListNode node1 = ListNode.newList(1,2);
 
         ListNode expected = new ListNode(1);
 
